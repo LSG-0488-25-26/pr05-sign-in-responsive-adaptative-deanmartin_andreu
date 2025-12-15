24a42691da422e9d61920418f414578b3e4f4a5c
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-12-15 15:20:17 +0100

    feat(ui): Creamos el Badge superior de la pantalla y añadimos el logo de la web.

--- app/src/main/java/com/example/responsive_adaptive_app/views/PantallaInicioSesion.kt
+++ app/src/main/java/com/example/responsive_adaptive_app/views/PantallaInicioSesion.kt
@@ -1,5 +1,6 @@
 package com.example.responsive_adaptive_app.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,20 +13,39 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.size
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.ui.res.painterResource
+import com.example.responsive_adaptive_app.R
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.ui.layout.ContentScale
 
 @Composable
 fun PantallaInicioSesion (navController: NavController) {
 
     Box(modifier = Modifier.fillMaxSize()) {
         Row(
-            modifier = Modifier.fillMaxWidth()
-                .padding(16.dp)
-                .align(Alignment.TopStart),
-            verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp)
+                .background(Color(16, 16, 15))
+                .statusBarsPadding()
+                .align(Alignment.TopCenter),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
-
+            Image(
+                painter = painterResource(id = R.drawable.logo_app_responsive),
+                contentDescription = "Logo web",
+                modifier = Modifier.height(120.dp)
+                    .padding(bottom = 14.dp)
+            )
         }
+
         Column (
             modifier = Modifier.fillMaxSize()
                 .padding(16.dp)
