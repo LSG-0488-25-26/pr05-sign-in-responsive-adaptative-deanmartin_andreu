f4879e125070c3174d1b1c005b65d8d568be7dd1
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-12-15 14:24:05 +0100

    feat(ui): Añadimos los contenedores de la pantalla para que el usuario Inicie sesión o se dirija a la pantalla para registrarse como usuario.

--- app/src/main/java/com/example/responsive_adaptive_app/views/PantallaInicioSesion.kt
+++ app/src/main/java/com/example/responsive_adaptive_app/views/PantallaInicioSesion.kt
@@ -1,9 +1,39 @@
 package com.example.responsive_adaptive_app.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.LineHeightStyle
 
 @Composable
 fun PantallaInicioSesion (navController: NavController) {
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
+                .align(Alignment.TopStart),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+        }
+        Column (
+            modifier = Modifier.fillMaxSize()
+                .padding(16.dp)
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
 }
