e23d47113e3462bdaaba3ee5e69b6e6977d78dd1
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-12-15 16:05:03 +0100

    feat(ui): Añadimos los composables que habrá en la pantalla de inciar sesión y los estilizamos.

--- app/src/main/java/com/example/responsive_adaptive_app/views/PantallaInicioSesion.kt
+++ app/src/main/java/com/example/responsive_adaptive_app/views/PantallaInicioSesion.kt
@@ -1,31 +1,45 @@
 package com.example.responsive_adaptive_app.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.ui.Alignment
-import androidx.compose.foundation.layout.size
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.ui.res.painterResource
-import com.example.responsive_adaptive_app.R
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.responsive_adaptive_app.R
+import androidx.compose.material3.TextFieldDefaults
 
 @Composable
 fun PantallaInicioSesion (navController: NavController) {
+
+    var emailUsuario by rememberSaveable { mutableStateOf("") }
+    var contrasena by rememberSaveable { mutableStateOf("") }
+    val botonHabilitado = emailUsuario.isNotBlank() && contrasena.isNotBlank()
 
     Box(modifier = Modifier.fillMaxSize()) {
         Row(
@@ -41,19 +55,97 @@ fun PantallaInicioSesion (navController: NavController) {
             Image(
                 painter = painterResource(id = R.drawable.logo_app_responsive),
                 contentDescription = "Logo web",
-                modifier = Modifier.height(120.dp)
+                modifier = Modifier
+                    .height(120.dp)
                     .padding(bottom = 14.dp)
             )
         }
 
         Column (
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
                 .padding(16.dp)
                 .align(Alignment.Center),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            TextField(
+                value=emailUsuario,
+                onValueChange = {emailUsuario=it},
+                label = {Text("Escribe tu email o nombre de usuario")},
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Black,
+                    unfocusedIndicatorColor = Color.DarkGray
+                )
+            )
 
+            Spacer(modifier = Modifier.height(12.dp))
+
+            TextField(
+                value = contrasena,
+                onValueChange = { contrasena = it },
+                label = { Text("Contraseña") },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Black,
+                    unfocusedIndicatorColor = Color.DarkGray
+                )
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    if(emailUsuario.isNotBlank() && contrasena.isNotBlank()) {}
+                },
+                enabled = botonHabilitado,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(42.dp),
+                shape = RoundedCornerShape(5.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.White,
+                    disabledContainerColor = Color.DarkGray,
+                    disabledContentColor = Color.LightGray,
+                )
+            ) {
+                Text(text = "Iniciar Sesión", fontSize = 16.sp)
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "¿No tienes cuenta? Pulsa aqui para registrarte.",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(42.dp)
+                    .shadow(elevation = 2.dp, shape = RoundedCornerShape(5.dp)),
+                shape = RoundedCornerShape(5.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                    contentColor = Color.Black,
+                )
+            ) {
+                Text("Registrarse")
+            }
         }
     }
 }
