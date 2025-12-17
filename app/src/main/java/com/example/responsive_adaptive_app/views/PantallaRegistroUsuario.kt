e0fa634998cd7f10f0626d5f0239d496618fe35a
Andreu <andreu.riba@gracia.lasalle.cat>
2025-12-17 23:42:27 +0100

    feat: añadir campos básicos de nombre y apellidos

--- app/src/main/java/com/example/responsive_adaptive_app/views/PantallaRegistroUsuario.kt
+++ app/src/main/java/com/example/responsive_adaptive_app/views/PantallaRegistroUsuario.kt
@@ -1,9 +1,52 @@
 package com.example.responsive_adaptive_app.views
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
-@Composable
-fun PantallaRegistroUsuario (navController: NavController) {
 
+@Composable
+fun PantallaRegistroUsuario(navController: NavController) {
+
+    var nombre by remember { mutableStateOf("") }
+    var apellido by remember { mutableStateOf("") }
+    var segundoApellido by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Registro de Usuario") })
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+
+            OutlinedTextField(
+                value = nombre,
+                onValueChange = { nombre = it },
+                label = { Text("Nombre") }
+            )
+
+            OutlinedTextField(
+                value = apellido,
+                onValueChange = { apellido = it },
+                label = { Text("1º Apellido") }
+            )
+
+            OutlinedTextField(
+                value = segundoApellido,
+                onValueChange = { segundoApellido = it },
+                label = { Text("2º Apellido") }
+            )
+        }
+    }
 }
